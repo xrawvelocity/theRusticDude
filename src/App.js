666a27aa1b0c7dc5150d5367ae71c943a4dd760c
Victor Fernandez <vmfs99@gmail.com>
2020-05-25 03:53:07 -0400

    working on mobile home

--- src/App.js
+++ src/App.js
@@ -8,13 +8,28 @@ import Faq from "./components/FAQ";
 import Reviews from "./components/Reviews";
 
 export default class App extends Component {
+  state = {
+    active: false,
+    close: false,
+  };
+
   render() {
     return (
       <Fragment>
         <nav className="navigation">
-          <Link to="/" className="navigation-logo">
-            <img className="navigation-logo_image" src="./images/theRusticDudeLogoImage.png" alt="logo" />
-            <img className="navigation-logo_text" src="./images/theRusticDudeLogoText.png" alt="logo" />
+          <Link onClick={()=>{
+              this.setState({ active: false, close: true})
+            }} to="/" className="navigation-logo">
+            <img
+              className="navigation-logo_image"
+              src="./images/theRusticDudeLogoImage.png"
+              alt="logo"
+            />
+            <img
+              className="navigation-logo_text"
+              src="./images/theRusticDudeLogoText.png"
+              alt="logo"
+            />
           </Link>
           <div className="navigation-links">
             <Link to="/projects" className="navigation-links_projects">
@@ -33,20 +48,51 @@ export default class App extends Component {
               FAQ
             </Link>
           </div>
-          <div className="navigation-hamburger">
-            <Link to="/projects" className="navigation-hamburger_projects">
+          <div
+            onClick={async () => {
+              await this.setState({
+                active: !this.state.active,
+                close: this.state.active ? true : false,
+              });
+            }}
+            className={!this.state.active ? "hamburger": "hamburger_open"}
+          >
+            <span className={!this.state.active ? "hamburger-one": "hamburger-one_open"}></span>
+            <span className={!this.state.active ? "hamburger-two": "hamburger-two_open"}></span>
+            <span className={!this.state.active ? "hamburger-three": "hamburger-three_open"}></span>
+          </div>
+          <div
+            className={
+              !this.state.active
+                ? `navigation-hamburger ${
+                    this.state.close ? "navigation-hamburger_close" : ""
+                  }`
+                : "navigation-hamburger_active"
+            }
+          >
+            <Link onClick={()=>{
+              this.setState({ active: false, close: true})
+            }} to="/projects" className="navigation-hamburger_projects">
               Projects
             </Link>
-            <Link to="/reviews" className="navigation-hamburger_reviews">
+            <Link onClick={()=>{
+              this.setState({ active: false, close: true})
+            }} to="/reviews" className="navigation-hamburger_reviews">
               Reviews
             </Link>
-            <Link to="/about" className="navigation-hamburger_about">
+            <Link onClick={()=>{
+              this.setState({ active: false, close: true})
+            }} to="/about" className="navigation-hamburger_about">
               About
             </Link>
-            <Link to="/contact" className="navigation-hamburger_contact">
+            <Link onClick={()=>{
+              this.setState({ active: false, close: true})
+            }} to="/contact" className="navigation-hamburger_contact">
               Contact
             </Link>
-            <Link to="/FAQ" className="navigation-hamburger_FAQ">
+            <Link onClick={()=>{
+              this.setState({ active: false, close: true})
+            }} to="/FAQ" className="navigation-hamburger_FAQ">
               FAQ
             </Link>
           </div>
@@ -54,11 +100,27 @@ export default class App extends Component {
         <main>
           <Switch>
             <Route exact path="/" render={(props) => <Home {...props} />} />
-            <Route exact path="/projects" render={(props) => <Projects {...props} />} />
-            <Route exact path="/about" render={(props) => <About {...props} />} />
-            <Route exact path="/contact" render={(props) => <Contact {...props} />} />
+            <Route
+              exact
+              path="/projects"
+              render={(props) => <Projects {...props} />}
+            />
+            <Route
+              exact
+              path="/about"
+              render={(props) => <About {...props} />}
+            />
+            <Route
+              exact
+              path="/contact"
+              render={(props) => <Contact {...props} />}
+            />
             <Route exact path="/Faq" render={(props) => <Faq {...props} />} />
-            <Route exact path="/reviews" render={(props) => <Reviews {...props} />} />
+            <Route
+              exact
+              path="/reviews"
+              render={(props) => <Reviews {...props} />}
+            />
           </Switch>
         </main>
       </Fragment>
