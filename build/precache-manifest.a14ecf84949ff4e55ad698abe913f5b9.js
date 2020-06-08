cf285842bf696ce4c94fa292179152cd5e497c36
Victor Fernandez <vmfs99@gmail.com>
2020-06-08 15:12:14 -0400

    build

---
+++ build/precache-manifest.a14ecf84949ff4e55ad698abe913f5b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f82608e57ad7edf2a4f4d86f0552ae4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cb4067d294b0b571c29",
+    "url": "/static/css/main.636dd0e5.chunk.css"
+  },
+  {
+    "revision": "91ace6eae62133bec7d0",
+    "url": "/static/js/2.770e104b.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.770e104b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb4067d294b0b571c29",
+    "url": "/static/js/main.7cee85e5.chunk.js"
+  },
+  {
+    "revision": "1bb895b2429444a647a3",
+    "url": "/static/js/runtime-main.09444656.js"
+  }
+]);
