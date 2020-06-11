e2c2707efb4ae4f737cdcb88d800bee237169d25
Victor Fernandez <vmfs99@gmail.com>
2020-06-10 20:53:34 -0400

    almost there

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,9 @@ export default class About extends Component {
     return (
       <div className="about">
         <iframe className="about-video" title="The Rustic Dude" src="https://www.youtube.com/embed/fRxSNFj7j38" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"></iframe>
-        
+        {/* <h1 className="about-text">We are a small business based in Miami, FL</h1>
+        <h1 className="about-text">We buy our wood from The Home Depot</h1>
+        <h1 className="about-text">We deliver straight to your home</h1> */}
       </div>
     );
   }
